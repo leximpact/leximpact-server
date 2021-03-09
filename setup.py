400f2fa8dd43fb65e802585c3f5905f9c5adfa58
sandcha <sandcha@users.noreply.github.com>
2021-03-09 12:12:13 +0100

    Fix common numpy version for all code and models

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ basic_requirements = [
     "flask-cors >= 3.0.7, < 3.1.0",
     "gunicorn >= 20.0.0, < 21.0.0",
     "mailjet-rest >= 1.3.3, < 2.0.0",
-    "numpy == 1.17",
+    "numpy == 1.18",
     "pandas >= 0.24.0, < 0.25.0",
     "psycopg2 >= 2.8.3, < 3.0.0",
     "pyjwt >= 1.7.1, < 2.0.0",
