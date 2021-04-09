1c66325decfa541194525e8f861b3404a133c0b6
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2021-04-09 10:33:32 +0200

    Env to hide warnings

--- server/app.py
+++ server/app.py
@@ -3,6 +3,9 @@ import os
 import connexion  # type: ignore
 from flask_cors import CORS as cors  # type: ignore
 import logging
+from dotenv import load_dotenv
+import warnings
+
 
 logging.basicConfig(level=logging.INFO)
 con = connexion.App("server", specification_dir=".")
@@ -12,4 +15,8 @@ cors(app, origins="*")
 
 
 if __name__ == "__main__":
+    load_dotenv(dotenv_path=".env")
+    # Do we have to remove warning ?
+    if os.environ.get("IGNORE_WARNING") == "yes":
+        warnings.filterwarnings("ignore")
     con.run(host=os.environ.get("HOST"), port=os.environ.get("PORT"))
