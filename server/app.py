ff4c8b809e17785ab02ee02c755f88e1544c28b9
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2021-04-19 14:13:45 +0200

    Prepare to hide openfisca warnings

--- server/app.py
+++ server/app.py
@@ -6,7 +6,7 @@ import logging
 from dotenv import load_dotenv
 import warnings
 from pandas.core.common import SettingWithCopyWarning  # type: ignore
-
+# Wait for openfisca-core upgrade : from openfisca_core.warnings import OpenFiscaMemoryWarning, OpenFiscaTempfileWarning  # type: ignore
 
 logging.basicConfig(level=logging.INFO)
 con = connexion.App("server", specification_dir=".")
@@ -21,4 +21,6 @@ if __name__ == "__main__":
     if os.environ.get("IGNORE_WARNING") == "yes":
         logging.warning('--- All SettingWithCopyWarning warnings from Pandas will be ignored ---')
         warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
+        # Wait for openfisca-core upgrade : warnings.simplefilter(action="ignore", category=OpenFiscaMemoryWarning)
+        # Wait for openfisca-core upgrade : warnings.simplefilter(action="ignore", category=OpenFiscaTempfileWarning)
     con.run(host=os.environ.get("HOST"), port=os.environ.get("PORT"))
